de0c9a1e6dcc724c76ffc800b2d6359b77ba7aa7
niezhaoxu123 <1445431738@qq.com>
2019-10-18 16:01:53 +0800

    123

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     这里gitflow练习
-    <Login></Login>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    Login
+    HelloWorld
   }
 }
 </script>
